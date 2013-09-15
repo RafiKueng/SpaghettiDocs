54237c8d33294918e0953d79b2366f11f3424eb3
RafiKueng <rafi.kueng@gmx.ch>
2013-09-15 19:39:58 +0200

    lmt.ui and model described

--- techdoc/code/src/js/lmt.events_part.js
+++ techdoc/code/src/js/lmt.events_part.js
@@ -1,9 +1,8 @@
-var events = {
+LMT.events = {
   startUp: function(){
     //...
     LMT.model = new LMT.objects.Model();
     LMT.model.init();
-    //...
     LMT.events.assignHandlers();
     //...
     },
@@ -12,11 +11,12 @@ var events = {
     // ...
     $(document).on('ShowSelectDatasourceDialog',
       LMT.ui.html.SelectDatasourceDialog.show);    
-    // ... 
+
+    $(document).on('RepaintModel', LMT.objects.Model.Repaint);
+
     $(document).on('UploadModel', LMT.com.UploadModel);
     $(document).on('SimulateModel', LMT.events.SimulateModel);
-    $(document).on('RepaintModel', LMT.objects.Model.Repaint);
-    //...
+
     $(document).on('ReceivedSimulation', LMT.ui.out.load);
     $(document).on('ReceivedSimulation', LMT.ui.html.Toolbar.updateTop);
     // ...
